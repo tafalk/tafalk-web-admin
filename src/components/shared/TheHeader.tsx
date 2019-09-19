bcff3c36c582c23b8273a3a69b028b008f242c15
100117 <vahdet.keskin@isbank.com.tr>
2019-09-19 17:28:07 +0300

    Initial commit

---
+++ src/components/shared/TheHeader.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Header, Button, Menu } from 'semantic-ui-react'
+
+const TheHeader: React.FC = () => {
+  return (
+    <Menu fixed='top' borderless>
+      <Menu.Item color='red' name='Tafalk! Admin'>
+        <Header as='h2' color='red'>
+          Tafalk! Admin
+        </Header>
+      </Menu.Item>
+
+      <Menu.Item position='right'>
+        <Button primary>LOGIN</Button>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default TheHeader
