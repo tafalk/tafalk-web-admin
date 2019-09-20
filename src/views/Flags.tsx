30fe20e97d23c049050e9c5941e248e2b8af9e4c
100117 <vahdet.keskin@isbank.com.tr>
2019-09-20 16:31:37 +0300

    Flag table dummy initialization

--- src/views/Flags.tsx
+++ src/views/Flags.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
-import { Segment } from 'semantic-ui-react'
+import { Table, Header } from 'semantic-ui-react'
 
 const FlagsView: React.FC = () => {
   return (
-    <Segment>Flags to be Here</Segment>
+    <div>
+      <Header as='h2' color='grey'>
+        Flags
+      </Header>
+      <Table color='olive'>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Id</Table.HeaderCell>
+            <Table.HeaderCell>Content Id</Table.HeaderCell>
+            <Table.HeaderCell>Flag Category</Table.HeaderCell>
+            <Table.HeaderCell>Flag Details</Table.HeaderCell>
+            <Table.HeaderCell>Status</Table.HeaderCell>
+            <Table.HeaderCell>Created</Table.HeaderCell>
+            <Table.HeaderCell>LastChange</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>1</Table.Cell>
+            <Table.Cell>1</Table.Cell>
+            <Table.Cell>Spam</Table.Cell>
+            <Table.Cell></Table.Cell>
+            <Table.Cell>RESOLVED</Table.Cell>
+            <Table.Cell>2019-09-19</Table.Cell>
+            <Table.Cell>2019-09-20</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
   )
 }
 
